0653e579cbdb7f0577fb070f48132865f808c46b
Danko Karleusa <dankomatichevich@gmail.com>
2022-03-17 17:45:40 -0700

    commit tree d905544dcc55a0882d918f3b430c5c9af38961c8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+const findMatching = (array, string) => {
+    const findMatch = (element) => {
+        return element.toLowerCase() === string.toLowerCase()
+    }
+    const match = array.filter(findMatch)
+    return match
+}
+// console.log(findMatching(drivers, "bobby"))
+
+const fuzzyMatch = (array, string) => {
+    const letterMatch = (element) => {
+        return element.startsWith(`${string}`)
+    }
+    const match = array.filter(letterMatch)
+    return match
+}
+// console.log(fuzzyMatch(drivers, "S"))
+
+const matchName = (array, string) => {
+    const objKeys = (element) => {
+        const scanName = element.name === string
+        return scanName
+    }
+    const match = array.filter(objKeys)
+    return match
+}
+
+
